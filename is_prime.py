38d6a685235ef4006ebade105db881598bb3c934
Prabhakar <prk327@gmail.com>
2017-06-17 21:27:26 +0530

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,38 +1,38 @@
 def is_prime(X):
+    p = 0
     if X < 0:
         p = 2
     else:
-            p = 0
-            l = []
-            y = 0
-            o = 0
-            m = [0,1]
-            for n in range(2,10):
-                if X == 0 or X == 1:
-                    return False
-                    break
-                elif X == 2 or X == 3 or X == 5 or X == 7:
-                    return True
-                    break
-                elif n > X:
-                    break
-                elif X % n == 0:
-                    p += 1
-                else:
-                    y = X % n
-                    l.append(y)
-            if p == 1:
-                for z in l:
-                    for n in range(2,10):
-                        if z not in m and z % n == 0:
-                            p += 1
-                            break
-                        elif n > z:
-                            break
-                        else:
-                            o = z % n
-                            l.append(o)
-                            del l[0]
+        l = []
+        y = 0
+        o = 0
+        m = [0,1]
+        for n in range(2,10):
+            if X == 0 or X == 1:
+                return False
+                break
+            elif X == 2 or X == 3 or X == 5 or X == 7:
+                return True
+                break
+            elif n > X:
+                break
+            elif X % n == 0:
+                p += 1
+            else:
+                y = X % n
+                l.append(y)
+        if p == 1:
+            for z in l:
+                for n in range(2,10):
+                    if z not in m and z % n == 0:
+                        p += 1
+                        break
+                    elif n > z:
+                        break
+                    else:
+                        o = z % n
+                        l.append(o)
+                        del l[0]           
     if p > 1:
         return False
     else:

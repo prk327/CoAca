df54b0a6b0b515d492f46ded8f9136427c488d80
Prabhakar <prk327@gmail.com>
2020-08-01 13:46:39 +0530

    Update 2darray.py

--- 2darray.py
+++ 2darray.py
@@ -13,3 +13,15 @@ array_1 = np.array([list_1,list_2])
 
 print(array_1.shape)
 print(array_1.ndim)
+
+'''2D Array
+Description
+From a 2D array extract all the rows of the 2 column.
+Hint: 2 column will have index value as 1.'''
+import ast,sys
+input_str = sys.stdin.read()
+input_list = ast.literal_eval(input_str)
+import numpy as np
+array_2d =np.array(input_list) 
+
+print(array_2d[ : ,1])

fb6316de330d58f0796b8f6e41a928f37d60fcbe
Prabhakar <prk327@gmail.com>
2020-08-01 13:42:16 +0530

    Create arange_fun.py

---
+++ arange_fun.py
@@ -0,0 +1,8 @@
+'''Array 'arange' Function
+Description
+Create an array of first 10 multiples of 5 using the 'arange' function.'''
+
+import numpy as np
+array_multipleof5 = np.arange(5,51,5)
+
+print(array_multipleof5)
